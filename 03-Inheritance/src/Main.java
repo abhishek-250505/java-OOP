cefbead1c33db640afb6608acf205026b9f7271f
Abhishek Anand <abhishek.664128@gmail.com>
2026-03-12 23:10:51 +0530

    solved inheritance problem

---
+++ 03-Inheritance/src/Main.java
@@ -0,0 +1,27 @@
+class Parents{
+    String name;
+    int age;
+
+    void displayInfo(){
+        System.out.println("Name:" + name);
+        System.out.println("Age :" + age);
+    }
+
+}
+class Child extends Parents{
+    int roll;
+    void display(){
+        System.out.println("Roll no :" + roll);
+    }
+}
+
+public class Main {
+    public static void main (String[] args){
+        Child obj1 = new Child();
+        obj1.name = "Abhishek ";
+        obj1.age= 22;
+        obj1.roll=101;
+        obj1.displayInfo();
+        obj1.display();
+    }
+}
